509096516a24f32c96fa5152fe3624915bcde495
Adan Luna <adan@oundmedia.com>
2018-06-27 21:12:00 -0500

    * Perfil: Verificar si ha llenado perfil de usuario en dashboard

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -3,35 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\ArangoDB;
-use Illuminate\Http\Request;
-use ArangoDBClient\Document as ArangoDocument;
-use ArangoDBClient\Exception as ArangoException;
-use ArangoDBClient\Export as ArangoExport;
-use ArangoDBClient\ConnectException as ArangoConnectException;
-use ArangoDBClient\ClientException as ArangoClientException;
-use ArangoDBClient\ServerException as ArangoServerException;
-use ArangoDBClient\Statement as ArangoStatement;
-use ArangoDBClient\UpdatePolicy as ArangoUpdatePolicy;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
 
 class PanelController extends Controller
 {
-    protected $DocumentHandler;
-    protected $CollectionHandler;
     protected $ArangoDB;
-    protected $EdgeHandler;
 
     public function __construct(ArangoDB $ArangoDB)
     {
         $this->ArangoDB = $ArangoDB;
-        $this->DocumentHandler = $ArangoDB->DocumentHandler();
-        $this->CollectionHandler = $ArangoDB->CollectionHandler();
-        $this->EdgeHandler = $ArangoDB->EdgeHandler();
         $this->middleware('is_user');
     }
 
     public function Index(){
-        return view('panel.dashboard');
+        //Obteniendo perfil
+        $data = $this->ArangoDB->Query('FOR doc IN perfiles FILTER doc.userKey == "'.auth()->user()->_key.'" RETURN doc');
+        if(count($data)<1){
+            return redirect()->action('PanelPerfiles@Index');
+        }else{
+            return view('panel.dashboard');
+        }
     }
 }
