6b52561d819e7e3180977c6869cc6d1b830eac1e
Adan Luna <adan@oundmedia.com>
2019-05-02 17:10:37 -0500

    Fix Upload Image Entidades

--- resources/views/admin/entidades/partial.blade.php
+++ resources/views/admin/entidades/partial.blade.php
@@ -22,7 +22,7 @@
                 <img src="https://imgplaceholder.com/240x250/37bc9b/ffffff/fa-file-photo-o?text=_none_&font-size=60" width="120" height="120" border="0" alt="" />
             @endif
         @endif
-        <input type='file' name="imagen" id="" accept=".jpg, .jpeg, .png, gif" style="margin-top: 20px; display: block;" class="" />
+        <input type='file' name="imagen" id="" accept=".jpg, .jpeg, .png, .gif" style="margin-top: 20px; display: block;" class="" />
         @if ($errors->has('imagen'))
             <span class="invalid-feedback" role="alert"><strong>{{ $errors->first('imagen') }}</strong></span>
         @endif
