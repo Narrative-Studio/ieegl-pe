f6cbb28ee51970a4f5192f14389bb1378d810b03
Adan Luna <adan@oundmedia.com>
2019-06-17 13:04:22 -0500

    Cambios con nayeli 11

--- resources/views/admin/convocatorias/partial.blade.php
+++ resources/views/admin/convocatorias/partial.blade.php
@@ -230,7 +230,7 @@
                             <label class="custom-control-label" for="a3">Para aprobación</label>
                         </div>
                         <div class="d-inline-block custom-control custom-radio mr-1">
-                            {!! Form::radio('activo', "cerrafda", null, ['id'=>'a4', 'class'=>'custom-control-input required']); !!}
+                            {!! Form::radio('activo', "cerrada", null, ['id'=>'a4', 'class'=>'custom-control-input required']); !!}
                             <label class="custom-control-label" for="a4">Cerrada</label>
                         </div>
                     </div>
